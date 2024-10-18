d2b94812b0676fa0c0eafb6d78f95e49611e34cc
Maubek8 <mauriciokraemer@gmail.com>
2024-10-18 18:29:43 -0300

    Update preloud.js

--- src/preloud.js
+++ src/preloud.js
@@ -1,11 +1,20 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expor APIs seguras para o contexto da página
-contextBridge.exposeInMainWorld('electron', {
-    sendMessage: (channel, data) => {
-        ipcRenderer.send(channel, data);
-    },
-    onMessage: (channel, callback) => {
-        ipcRenderer.on(channel, (event, ...args) => callback(...args));
+// Exposing safe APIs to the renderer process
+contextBridge.exposeInMainWorld('electronAPI', {
+  sendToMain: (channel, data) => {
+    // whitelist channels to prevent exposing all IPC channels
+    const validChannels = ['toMain'];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
     }
+  },
+  receiveFromMain: (channel, func) => {
+    const validChannels = ['fromMain'];
+    if (validChannels.includes(channel)) {
+      // Remove the listener if exists to avoid memory leaks
+      ipcRenderer.removeAllListeners(channel);
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  }
 });
