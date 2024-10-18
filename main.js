b1df61a997b63da485d69dd6d3e7022d5deb45eb
Maubek8 <mauriciokraemer@gmail.com>
2024-10-18 10:50:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,25 +12,26 @@ function createWindow() {
         }
     });
 
-    // Carrega a página de login como primeira página
+    // Carregar a página de login ao iniciar
     mainWindow.loadFile(path.join(__dirname, 'src/html/login.html'));
 
-    // Abrir DevTools (opcional, para desenvolvimento)
-    mainWindow.webContents.openDevTools();
-
+    // Se a janela for fechada
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
 }
 
+// Quando o Electron estiver pronto para iniciar
 app.on('ready', createWindow);
 
+// Fechar o aplicativo quando todas as janelas forem fechadas
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
+// Criar nova janela se o aplicativo for ativado novamente
 app.on('activate', function () {
     if (mainWindow === null) {
         createWindow();
