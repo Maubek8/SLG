f4ce8c1a21823101446962759f1919ef89aa0085
Maubek8 <mauriciokraemer@gmail.com>
2024-10-18 10:34:42 -0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
+function login() {
+  // Simulação de autenticação
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username === 'admin' && password === '1234') {
+    window.location.href = 'dashboard.html'; // Redireciona para o Dashboard
+  } else {
+    alert('Usuário ou senha inválidos');
+  }
+}
 
